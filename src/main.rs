2c29666bf61822eb81a7d83f5558decdb40f452a
John Vivian <jtvivian@gmail.com>
2018-12-19 01:36:53 -0800

    First crack at first implementation method

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap;
 use clap::{App, Arg, ArgMatches};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader, BufWriter, Result, Write};
 use std::path::Path;
 
 
@@ -13,16 +13,8 @@ struct PartialRead {
     qscore: String,
 }
 
-impl PartialRead {
-    fn new() -> PartialRead {
-        PartialRead {
-            seq: String::new(),
-            qscore: String::new(),
-        }
-    }
-}
-
-// Hold a complete Read which
+// Complete Read with header
+#[derive(PartialEq)]
 struct Read {
     header: String,
     seq: String,
@@ -40,14 +32,55 @@ impl Read {
 }
 
 // Give ownership of HashMap so I can pop items as pairs are found and it's not used elsewhere
-fn write_pairs(r2_path: &str, map: HashMap<String, PartialRead>) -> Result<()> {
+fn write_pairs(r2_path: &str, map: &mut HashMap<String, PartialRead>) -> Result<()> {
+    // Create output paths
+    let parent = Path::new(r2_path).parent().unwrap();
+    let r1_out_path = parent.join("R1_paired.fastq").to_str().unwrap().to_string(); // This is sort of ridiculous
+    let r2_out_path = parent.join("R2_paired.fastq").to_str().unwrap().to_string();
+    let singleton_path = parent.join("Singletons.fastq").to_str().unwrap().to_string();
+    // Create BufWriter objects for R1/R2
+    let r1_out_handle = File::create(r1_out_path)?;
+    let r2_out_handle = File::create(r2_out_path)?;
+    let singleton_handle = File::create(&singleton_path)?;
+    let mut r1_writer = BufWriter::new(r1_out_handle);
+    let mut r2_writer = BufWriter::new(r2_out_handle);
+    let mut singleton_writer = BufWriter::new(singleton_handle);
     // Open R2 and iterate over reads
+    let r2_handle = File::open(r2_path)?;
+    let mut reader = BufReader::new(r2_handle);
+    loop {
+        let read = match parse_read(&mut reader) {
+            Some(i) => i,
+            None => break
+        };
 
-    // Check if header is in hashmap
+        // Check if header is in hashmap
+        let header = parse_header(&read.header)?;
+        if map.contains_key(&header) {
+            // Write to BufWriters and remove from HashMap
+            let r1 = &map[&header];
+            write!(&mut r1_writer, "{}.1\n{}+\n{}", &header, r1.seq, r1.qscore)?;
+            write!(&mut r2_writer, "{}.2\n{}\n+\n{}", &header, read.seq, read.qscore)?;
+            map.remove(&header);
+        } else {
+            // Else: Write out R2 to singleton file
+            write!(&mut singleton_writer, "{}.2\n{}\n+\n{}", &header, read.seq, read.qscore)?;
+        }
+    }
 
-    // If in hashmap, write to BufWriter
+    // Write out remainder of singletons left in R1
+    for key in map.keys() {
+        let r1 = &map[key];
+        write!(&mut r1_writer, "{}.1\n{}+\n{}", &key, r1.seq, r1.qscore)?;
+    }
 
-    // Else: Write out singleton file
+    // If singleton's file is empty, delete
+    let s_handle = File::open(&singleton_path)?;
+    let mut s_reader = BufReader::new(s_handle);
+    match parse_read(&mut s_reader) {
+        Some(_) => {},
+        None => std::fs::remove_file(singleton_path)?
+    };
 
     Ok(())
 }
@@ -63,13 +96,20 @@ fn store_read1(r1_path: &str) -> Result<HashMap<String, PartialRead>> {
             None => break
         };
         // Parse header into unique String and store along with PartialRead in HashMap
-        let header_vec: Vec<&str> = read.header.split_whitespace().collect();
-        let uniq = header_vec[0].to_string()[0..header_vec[0].len() - 2].to_string();
-        map.insert(uniq, PartialRead { seq: read.seq, qscore: read.qscore });
+        let header = parse_header(&read.header)?;
+        map.insert(header, PartialRead { seq: read.seq, qscore: read.qscore });
     }
     Ok(map)
 }
 
+fn parse_header(header: &str) -> Result<String> {
+    let header_vec: Vec<&str> = header.split_whitespace().collect();
+    let uniq = header_vec[0][0..header_vec[0].len() - 2].to_string();
+    Ok(uniq)
+}
+
+
+/// Parses BufReader object to Read
 fn parse_read(file: &mut BufReader<File>) -> Option<Read> {
     let mut sep = String::new();
     let mut read = Read::new();
@@ -78,7 +118,6 @@ fn parse_read(file: &mut BufReader<File>) -> Option<Read> {
     file.read_line(&mut read.seq).ok()?;
     file.read_line(&mut sep).ok()?;
     file.read_line(&mut read.qscore).ok()?;
-    // Parse header for unique component
     Some(read)
 }
 
@@ -101,7 +140,7 @@ fn get_matches() -> ArgMatches<'static> {
                 .short("2")
                 .long("read2")
                 .value_name("PATH")
-                .required(false)
+                .required(true)
                 .help("Path to Read1 FASTQ")
                 .takes_value(true))
         .get_matches();
@@ -116,12 +155,11 @@ fn main() -> Result<()> {
 
     // Unwrap is safe here due to r1/r2 being required arguments
     let r1_path = matches.value_of("r1").unwrap();
-    //let r2_path = matches.value_of("r2").unwrap();
+    let r2_path = matches.value_of("r2").unwrap();
 
     // Pair fastqs
-    let reads = store_read1(&r1_path)?;
-
-    println!("{:?}", reads["@SRR3380692.1"]);
+    let mut reads = store_read1(&r1_path)?;
+    write_pairs(&r2_path, &mut reads)?;
 
     Ok(())
 }
